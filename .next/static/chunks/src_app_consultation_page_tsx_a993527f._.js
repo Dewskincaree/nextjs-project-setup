9830af50b125059065f023e41d20fee2f51a923a
Dew Skincare <76748990+Dewskincaree@users.noreply.github.com>
2025-07-16 13:06:14 +0300

    Add .next/static/chunks/src_app_consultation_page_tsx_a993527f._.js 1752664120021

---
+++ .next/static/chunks/src_app_consultation_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_badf02e2._.js",
+  "static/chunks/src_69b2fd98._.js"
+],
+    source: "dynamic"
+});
